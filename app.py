58c199a032ef414f0da32f18c5415801c90f3b85
ridhwanmuttaqien <133610677+ridhwanmuttaqien@users.noreply.github.com>
2023-08-09 07:44:26 +0700

    add source code deployment

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+
+st.set_page_config(
+    page_title='Predict',
+    layout='wide',
+    initial_sidebar_state='expanded'
+)
+
+#load model
+best_model = load_model('coffee_predict.h5')
+
+
+def img_predict(img, model):
+    pred = np.array(img)[:, :, :3]
+    pred = tf.image.resize(pred, size=(224, 224))
+    pred = pred / 255.0
+
+    
+    predicted_probabilities = model.predict(x=tf.expand_dims(pred, axis=0))[0]
+
+    
+    predicted_class_index = np.argmax(predicted_probabilities)
+
+    
+    if predicted_class_index == 0:
+        return "dark"
+    elif predicted_class_index == 1:
+        return "green"
+    elif predicted_class_index == 2:
+        return "light"
+    else:
+        return "medium"
+
+
+
+
+    
+
+def run():
+    # variable image
+    img = None
+
+    # Image upload and prediction
+    uploaded_img = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+    if uploaded_img is not None:
+        img = Image.open(uploaded_img)
+        prediction = img_predict(img, best_model)
+
+        # Display the prediction result
+        title = f"<h2 style='text-align:center'>{prediction}</h2>"
+        st.markdown(title, unsafe_allow_html=True)
+        st.image(img, use_column_width=True)
+
+if __name__ == "__main__":
+    run()
